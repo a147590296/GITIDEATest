9c82d75b0533a15d7ccbb86ca3f56448ae75108e
hrx <327481003@qq.com>
2021-02-24 10:28:59 +0800

    1

---
+++ src/main/java/com/atguigu/boot/TemplatesTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.boot;
+
+public class TemplatesTest {
+
+
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/com/atguigu/boot/controller/Test.java
@@ -0,0 +1,16 @@
+package com.atguigu.boot.controller;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
+        System.out.println(444);
+        System.out.println(555);
+        System.out.println(666);
+        HashMap<Object, Object> aa = new HashMap<>();
+
+    }
+}
